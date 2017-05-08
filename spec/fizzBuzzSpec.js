bdcaf4dfd1cba1991047076754397e0952064b97
Sophie Mann <sklm@live.co.uk>
2017-05-08 16:00:18 +0100

    returns fizzbuzz for 15 and number for all other numbers

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,10 +1,19 @@
 describe("fizzBuzz", function() {
 
-  it("returns fizz when passed 3", function() {
+  it("returns fizz when passed number divisible by 3", function() {
     expect(fizzBuzz(3)).toEqual("fizz");
   });
 
-  it('returns buzz when passed 5', function() {
+  it('returns buzz when passed number divisible by 5', function() {
     expect(fizzBuzz(5)).toEqual('buzz');
   });
+
+  it('returns fizzbuzz when passed number divisible by 15', function() {
+    expect(fizzBuzz(15)).toEqual('fizzbuzz');
+  });
+
+  it('returns the given number when not divisble by 3 or 5', function() {
+    expect(fizzBuzz(4)).toEqual(4);
+  });
+
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,4 +1,10 @@
 function fizzBuzz(number){
+  if (number % 15 === 0 ) {return 'fizzbuzz'};
   if (number % 3 === 0) {return 'fizz'};
   if (number % 5 === 0) {return 'buzz'};
+  return number;
 }
+
+for (var i = 1; i < 100; i++) {
+  console.log(fizzBuzz(i));
+};
