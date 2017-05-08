bf0f74a9be1e950d6dbbd63b2d0803a047e24989
Sophie Mann <sklm@live.co.uk>
2017-05-08 15:16:28 +0100

    initialise and add first fizz spec test

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,6 @@
+describe("fizzBuzz", function() {
+
+  it("returns fizz when passed 3", function() {
+    expect(fizzBuzz(3)).toEqual("fizz");
+  });
+});
---
+++ src/fizzBuzz.js
@@ -0,0 +1,3 @@
+function fizzBuzz(number){
+  return "fizz";
+}
